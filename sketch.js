a118b981d9f936b1a6c1610c5dc2f29ade5dfee4
Abhinay06 <70752773+Abhinay06@users.noreply.github.com>
2020-10-02 16:53:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -40,7 +40,7 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImage);
+    background(0);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
